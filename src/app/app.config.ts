b54bae20248d9167f38874a16e5e4a8778cf245f
lucaslopescunha <lucaslopescunha@gmail.com>
2026-03-02 19:45:37 -0300

    Class interceptor.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
+import { LoggingInterceptor } from './logging-interceptor';
 
 function loggingInterceptor(
   request: HttpRequest<unknown>, 
@@ -19,8 +20,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(
-      withInterceptors([loggingInterceptor])
+      withInterceptors([loggingInterceptor]),
+      withInterceptorsFromDi()
     ),
+    {provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true},
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
   ]
---
+++ src/app/logging-interceptor.ts
@@ -0,0 +1,12 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log('Request URL CLAZZ: '+req.url);
+        return next.handle(req);
+    }
+
+}
