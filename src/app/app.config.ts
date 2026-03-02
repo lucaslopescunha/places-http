f62584b6e7007e685fad6a6344ecfb5a9431366d
lucaslopescunha <lucaslopescunha@gmail.com>
2026-03-02 19:32:07 -0300

    Using interceptors.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+
+function loggingInterceptor(
+  request: HttpRequest<unknown>, 
+  next: HttpHandlerFn) {
+    const req = request.clone({
+      headers: request.headers.set('X-DEBUG', 'TESTING')
+    })
+    console.log('[Outgoing Request]');
+    console.log(req);
+    return next(req);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([loggingInterceptor])
+    ),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
   ]
